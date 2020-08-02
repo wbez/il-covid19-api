a0bcecad8bc64311e29feae217c0f7e496cfe5f4
pjsier <pjsier@gmail.com>
2020-08-02 10:25:00 -0500

    Add state recovery, testing data

---
+++ hasura/migrations/1596380258752_state_tables/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE public.state_race_counts;
+DROP TABLE public.state_age_counts;
+DROP TABLE public.state_age_race_counts;
+DROP TABLE public.state_gender_counts;
+DROP TABLE public.state_testing_results;
+DROP TABLE public.state_recovery_data;
+DROP TABLE public.state_probable_case_counts;
